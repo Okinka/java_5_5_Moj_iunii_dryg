04ed2ac586b959dd2057a8b84b8ba628a1accdb4
Okinka <olenkakinka1995@gmail.com>
2021-10-25 20:25:27 +0200

    Task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -3,8 +3,10 @@ package it.campe;
 public class Main {
 
     public static void main(String[] args) {
-        int a=2021, b=22, c=a-b;
-        System.out.println(" Год рождения друга" );
-        System.out.println( c );
+        int a=2021;
+        int b=22;
+        int c=a-b;
+        System.out.println("Год рождения друга");
+        System.out.println(c);
     }
 }
